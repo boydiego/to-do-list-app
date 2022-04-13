6cd6acbc556459999eb85cc2c513f0857155b426
diego boy <diegolive5000@gmail.com>
2022-04-13 17:33:35 +0200

    cross or uncross items when double clicked

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,33 @@
 // selecting elements
-let list = $('#list');
 let addButton = $('#button');
 
 // callback functions
 function addToDoItem() {
+  // CREATING ITEMS
   let inputValue = $('#input').val();
+  let list = $('#list');
 
-  // creating elements
-  let listItem = $('<li></li>')
+  let listItem = $('<li></li>');
+  let crossOutButton = $('<button>X</button>');
 
-  listItem.append(inputValue);
+  listItem.append(inputValue).append(crossOutButton);
   list.append(listItem);
+
+  // DELETING ITEMS
+  function deleteItem() {
+    crossOutButton.parent().remove();
+  }
+
+  crossOutButton.on('click', deleteItem);
+
+  // CROSSING OUT ITEMS
+  function crossOutItem() {
+    $(this).toggleClass('strike');
+  }
+
+  listItem.on('dblclick', crossOutItem);
 }
 
 // event listeners
 addButton.on('click', addToDoItem);
+
